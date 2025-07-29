4f81a17f3650bb3e848a97d64fb6425d9bc7306b
aritra0085 <aritrabiswas0085@gmail.com>
2025-07-29 16:01:34 +0530

    add animations

---
+++ app/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        border: "var(--border)",
+        ring: "var(--ring)",
+      },
+    },
+  },
+  plugins: [],
+};
--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -2,7 +2,7 @@ import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@cl
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./button";
-import { LayoutDashboard } from "lucide-react";
+import { LayoutDashboard, PenBox } from "lucide-react";
 
 const Header = () => {
   return (
@@ -16,12 +16,20 @@ const Header = () => {
       className="h-12 w-auto object-contain"
       />
       </Link>
-      <div>
-        <SignedIn>
-          <Link href={"/dashboard"}>
-            <Button variant="outline">
-              <LayoutDashboard size={18}/>
-              <span>Dashboard</span>
+      <div className="flex items-center space-x-4">
+            <SignedIn>
+              <Link href="/dashboard" className="text-gray-600 hover:text-blue-600 flex items-center gap-2">
+              <Button variant="outline">
+                <LayoutDashboard size={18} />
+                <span className="hidden md:inline">Dashboard</span>
+              </Button>
+            </Link>
+
+
+          <Link href={"/transaction/create"}>
+            <Button className="flex item-center gap-2">
+              <PenBox size={18}/>
+              <span className="hidden md:inline">Add Transaction</span>
             </Button>
           </Link>
         </SignedIn>
@@ -32,7 +40,13 @@ const Header = () => {
         </SignInButton>
       </SignedOut>
       <SignedIn>
-        <UserButton />
+        <UserButton
+         appearance={{
+           elements: {
+            avatarBox: "w-10 h-10",
+          },
+        }}
+      />
       </SignedIn>
       </div>
       </nav>
